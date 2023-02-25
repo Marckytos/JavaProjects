071b1787717a261bd2bf69372081d86af0af0c12
Marckytos <marquillos.bros3451@gmail.com>
2023-02-24 18:45:19 -0600

    Alumno.java

---
+++ Alumno.java
@@ -0,0 +1,35 @@
+public class Alumno{
+    private String Nombre;
+    private int Edad;
+    private String carrera;
+    private int semestre;
+}
+//Metodo constructor por defecto
+public Alumno(){}
+public void setNombre(String n){
+    this.Nombre = n;
+
+}
+
+public String getNombre(){ return this.Nombre; }
+
+public void setEdad(int e){
+    this.Edad=e;
+}
+public int getEdad(){ return this.Edad; }
+public String tostring(){
+    String edo = "Nombre "+ Nombre +"\n";
+    edo= edo +"Edad "+ Edad;
+    return edo;
+}
+public int estudiar(String tema){
+    System.out.print(this.Nombre + "Estudia");
+    System.out.println(tema);
+    return 1;
+}
+public int programar(String tema){
+    System.out.print(this.Nombre + "Esta programando");
+    return 2;
+}
+
+
---
+++ AlumnoDriver.java
@@ -0,0 +1,12 @@
+public class AlumnoDriver {
+    public static void main(String[] args){
+        Alumno al1 = new Alumno();
+        System.out.println(al1.toString());
+
+        al1.estudiar("calculo");
+        al1.setNombre("Juan");
+        al1.estudiar("Calculo");
+    
+    
+}
+}
