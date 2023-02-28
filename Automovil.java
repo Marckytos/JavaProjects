d4f0f8e470d4b6f72bf78c33f64105120ce36619
Marckytos <marquillos.bros3451@gmail.com>
2023-02-28 12:40:08 -0600

    Automovil.java

---
+++ Automovil.java
@@ -0,0 +1,14 @@
+public class Automovil {
+
+    private String marca;
+    private String subMarca;
+    private byte modelo; // 2000 + byte
+    private Color color;
+
+    public Automovil() { //Constructor por defecto
+    }
+
+    
+
+}
+
