b54841159fe3402f10c16b5bbb6c846783bc1eb3
Marckytos <marquillos.bros3451@gmail.com>
2023-03-01 19:48:20 -0600

    Automovil.java

--- Automovil.java
+++ Automovil.java
@@ -1,9 +1,11 @@
+import javax.swing.plaf.synth.ColorType;
+
 public class Automovil {
 
     private String marca;
     private String subMarca;
     private byte modelo; // 2000 + byte
-    private Color color;
+    private ColorType color;
 
     public Automovil() { //Constructor por defecto
     }
---
+++ AutomovilDriver.java
@@ -0,0 +1,8 @@
+public class AutomovilDriver {
+    public static void main(String[] args) {
+        Automovil auto1 = new Automovil();
+        Automovil auto2 = new Automovil();
+        System.out.println("El auto es:");
+    }
+    
+}
